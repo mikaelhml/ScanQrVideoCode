7bf1df9166915d3eb7544fa32a9bf2295476091e
Frederico <fglsm92@gmail.com>
2019-08-16 23:31:01 -0300

    onActivityResult incompleta

--- app/src/main/java/br/iesb/scanqrvideocode/MainActivity.java
+++ app/src/main/java/br/iesb/scanqrvideocode/MainActivity.java
@@ -1,19 +1,17 @@
 package br.iesb.scanqrvideocode;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
@@ -23,7 +21,6 @@ import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.qrcode.QRCodeWriter;
 
 import java.io.ByteArrayOutputStream;
-import java.util.stream.Stream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -70,25 +67,25 @@ public class MainActivity extends AppCompatActivity {
 
     private void gerarQR(byte[] byteArray) {
         QRCodeWriter writer = new QRCodeWriter();
-        String string = " teste teste teste teste teste teste teste teste  teste teste teste teste teste teste teste teste  teste teste teste teste teste teste teste teste  teste teste teste teste teste teste teste teste  teste teste teste teste teste teste teste teste  teste teste teste teste teste teste teste teste  teste teste teste t"
+        String string = " teste teste teste teste teste teste teste teste  teste teste teste teste teste teste teste teste  teste teste teste teste teste teste teste teste  teste teste teste teste teste teste teste teste  teste teste teste teste teste teste teste teste  teste teste teste teste teste teste teste teste  teste teste teste t";
 
         try {
-            BitMatrix bitMatrix = writer.encode(string, BarcodeFormat.QR_CODE, 512, 512)
-            int width = 512
-            int height = 512
-            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+            BitMatrix bitMatrix = writer.encode(string, BarcodeFormat.QR_CODE, 512, 512);
+            int width = 512;
+            int height = 512;
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
             for (int x=0; x < width; x++) {
                 for (int y=0;y<height;y++){
                     if (!bitMatrix.get(x, y))
-                        bmp.setPixel(x, y, Color.WHITE)
+                        bmp.setPixel(x, y, Color.WHITE);
                     else
-                        bmp.setPixel(x, y, Color.BLACK)
+                        bmp.setPixel(x, y, Color.BLACK);
                 }
             }
 
         } catch (WriterException e) {
             //Log.e("QR ERROR", ""+e);
-            Toast.makeText(this,"Fuck",Toast.LENGTH_LONG)
+            Toast.makeText(this,"Fuck",Toast.LENGTH_LONG);
         }
     }
 
@@ -140,33 +137,33 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+*/
 
 
 
     //
 
 
-    private void onActivityResult(Int requestCode, Int resultCode, data: Intent?) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
-        if (requestCode == 111 && resultCode == RESULT_OK) {
-            val selectedFile = data?.data //The uri with the location of the file
-            val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedFile)
-            val string64 = convert(bitmap);
-            criarQRCode(string64,true)
+        if ((requestCode == 111) && (resultCode == RESULT_OK)) {
+            Uri selectedFile = data.getData(); //The uri with the location of the file
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedFile);
+            byte[] string64 = convert(bitmap);
+            criarQRCode(string64,true);
         }
-        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
 
         if (result != null) {
             if (result.contents == null) {
-                Toast.makeText(this, "Está vazio", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Está vazio", Toast.LENGTH_LONG).show();
             } else {
 
 
-                val options = BitmapFactory.Options()
-                val bitmap = BitmapFactory.decodeByteArray(result.contents.toByteArray(), 0, result.contents.toString().length, options)
-                img_QRCode.setImageBitmap(bitmap)
-                Toast.makeText(this, result.contents.toString(), Toast.LENGTH_LONG).show()
+                val options = BitmapFactory.Options();
+                val bitmap = BitmapFactory.decodeByteArray(result.contents.toByteArray(), 0, result.contents.toString().length, options);
+                img_QRCode.setImageBitmap(bitmap);
+                Toast.makeText(this, result.contents.toString(), Toast.LENGTH_LONG).show();
 
 
             }
@@ -174,8 +171,8 @@ public class MainActivity extends AppCompatActivity {
         } else {
 
         }
-        super.onActivityResult(requestCode, resultCode, data)
-    }*/
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
 
 

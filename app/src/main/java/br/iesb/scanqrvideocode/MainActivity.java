ce7b51c16accdc53b933fb19b3b8a88856b2bad0
Frederico <fglsm92@gmail.com>
2019-08-16 21:57:58 -0300

    atualizacao

--- app/src/main/java/br/iesb/scanqrvideocode/MainActivity.java
+++ app/src/main/java/br/iesb/scanqrvideocode/MainActivity.java
@@ -3,14 +3,23 @@ package br.iesb.scanqrvideocode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.qrcode.QRCodeWriter;
 
 import java.io.ByteArrayOutputStream;
@@ -62,22 +71,22 @@ public class MainActivity extends AppCompatActivity {
     private void gerarQR(byte[] byteArray) {
         QRCodeWriter writer = new QRCodeWriter();
         String string = " teste teste teste teste teste teste teste teste  teste teste teste teste teste teste teste teste  teste teste teste teste teste teste teste teste  teste teste teste teste teste teste teste teste  teste teste teste teste teste teste teste teste  teste teste teste teste teste teste teste teste  teste teste teste t"
-        img_QRCode as ImageView
+
         try {
-            val bitMatrix = writer.encode(string, BarcodeFormat.QR_CODE, 512, 512)
-            val width = 512
-            val height = 512
-            val bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
-            for (x in 0 until width) {
-                for (y in 0 until height) {
+            BitMatrix bitMatrix = writer.encode(string, BarcodeFormat.QR_CODE, 512, 512)
+            int width = 512
+            int height = 512
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+            for (int x=0; x < width; x++) {
+                for (int y=0;y<height;y++){
                     if (!bitMatrix.get(x, y))
                         bmp.setPixel(x, y, Color.WHITE)
                     else
                         bmp.setPixel(x, y, Color.BLACK)
                 }
             }
-            img_QRCode.setImageBitmap(bmp)
-        } catch (e: WriterException) {
+
+        } catch (WriterException e) {
             //Log.e("QR ERROR", ""+e);
             Toast.makeText(this,"Fuck",Toast.LENGTH_LONG)
         }
@@ -97,6 +106,82 @@ public class MainActivity extends AppCompatActivity {
         btnCriarQRCode = findViewById(R.id.btnCriarQRCode);
         btnScanQRCode = findViewById(R.id.btnScanQRCode);
     }
+
+    //
+
+
+
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent resultIntent) {
+        // Check which request it is that we're responding to
+        if (requestCode == 111) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // Get the URI that points to the selected contact
+                Uri contactUri = resultIntent.getData();
+                // We only need the NUMBER column, because there will be only one row in the result
+                String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+                // Perform the query on the contact to get the NUMBER column
+                // We don't need a selection or sort order (there's only one result for the given URI)
+                // CAUTION: The query() method should be called from a separate thread to avoid blocking
+                // your app's UI thread. (For simplicity of the sample, this code doesn't do that.)
+                // Consider using <code><a href="/reference/android/content/CursorLoader.html">CursorLoader</a></code> to perform the query.
+                Cursor cursor = getContentResolver()
+                        .query(contactUri, projection, null, null, null);
+                cursor.moveToFirst();
+
+                // Retrieve the phone number from the NUMBER column
+                int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String number = cursor.getString(column);
+
+                // Do something with the phone number...
+            }
+        }
+    }
+
+
+
+
+
+    //
+
+
+    private void onActivityResult(Int requestCode, Int resultCode, data: Intent?) {
+
+        if (requestCode == 111 && resultCode == RESULT_OK) {
+            val selectedFile = data?.data //The uri with the location of the file
+            val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, selectedFile)
+            val string64 = convert(bitmap);
+            criarQRCode(string64,true)
+        }
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Está vazio", Toast.LENGTH_LONG).show()
+            } else {
+
+
+                val options = BitmapFactory.Options()
+                val bitmap = BitmapFactory.decodeByteArray(result.contents.toByteArray(), 0, result.contents.toString().length, options)
+                img_QRCode.setImageBitmap(bitmap)
+                Toast.makeText(this, result.contents.toString(), Toast.LENGTH_LONG).show()
+
+
+            }
+
+        } else {
+
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }*/
+
+
+
+
+
+    //
 
     private void inicitFunction() {
 
